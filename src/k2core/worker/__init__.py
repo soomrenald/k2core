8d341f159c009f28b40b720f52be501acaa3ed85
Wolfhard <jock.wolfhard@gmail.com>
2026-07-22 19:25:23 -0700

    Extract shared Krea runtime and worker protocol

---
+++ src/k2core/worker/__init__.py
@@ -0,0 +1,6 @@
+"""Isolated GPU worker protocol."""
+
+from k2core.worker.protocol import CommandKind, WorkerCommand, WorkerEvent, WorkerState
+
+__all__ = ["CommandKind", "WorkerCommand", "WorkerEvent", "WorkerState"]
+
